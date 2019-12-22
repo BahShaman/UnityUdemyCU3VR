e4cacf41f6155c69a852b96d977ab9573959f4db
Mike B <mikeboscia@gmail.com>
2019-12-21 22:11:32 -0800

    finished 48

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,30 +6,49 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
     
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //print("hello");
-        ProcessInput();
+        rigidBody.freezeRotation = true;
+
+        HandleThrust();
+        HandleRotate();
+
+        rigidBody.freezeRotation = false;
     }
 
-    private void ProcessInput()
+    private void HandleRotate()
     {
-        
+        HandleLeftRotation(); //not used, and held by rigidbody constraints
+        HandleForwardRotation();
+    }
 
-        if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
+    private void HandleForwardRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
         {
-            //print("Trusting");
-            rigidBody.AddRelativeForce(Vector3.up);
-        }
+            //print("Rotate right");
+            transform.Rotate(Vector3.forward);
 
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+            //print("Rotate left");
+        }
+    }
+
+    private void HandleLeftRotation()
+    {
         if (Input.GetKey(KeyCode.W))
         {
             //print("Rotate forward");
@@ -41,18 +60,22 @@ public class Rocket : MonoBehaviour
             transform.Rotate(-Vector3.left);
             //print("Rotate back");
         }
+    }
 
-
-        if (Input.GetKey(KeyCode.A))
+    private void HandleThrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
         {
-            //print("Rotate right");
-            transform.Rotate(Vector3.forward);
-
-        }else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward);
-            //print("Rotate left");
+            //print("Trusting");
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
-
+        else
+        {
+            audioSource.Stop();
+        }
     }
 }
